293641de69daa2fac8657cb9fbca4ae81fc8bbe5
Muhammed Abdelhadi <mano299ahmed1@gmail.com>
2026-08-27 14:29:40 +0300

    add assets generator

---
+++ lib/core/generated/app_asssets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for imagesSplashHead
+  /// assets/images/splash_head.svg
+  static const String imagesSplashHead = "assets/images/splash_head.svg";
+
+  /// Assets for imagesSplashIcon
+  /// assets/images/splash_icon.svg
+  static const String imagesSplashIcon = "assets/images/splash_icon.svg";
+}
+
